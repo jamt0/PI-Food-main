c89f0297f57c2b724fe9f1bb8e027d3fa4050923
lilieth <liliethramirez.9@gmail.com>
2022-11-11 22:01:20 -0300

    Changes to be committed: 	modified:   api/src/controllers/diets.js

--- api/src/controllers/diets.js
+++ api/src/controllers/diets.js
@@ -1,27 +1,27 @@
 const axios = require("axios");
-// const { YOUR_API_KEY } = process.env;
-// const { Diet } = require('../db');
+ const { YOUR_API_KEY } = process.env;
+const { Diet } = require('../db');
 
 
 const getDiets = async function () {
-    // const dieta = await Diet.findAll()
-    // if(!dieta.length){
-    const apiDiet = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=659b1bcda0a44b3693ac4192d1b8c5ac&addRecipeInformation=true&number=100`);
+    const dieta = await Diet.findAll()
+    if(!dieta.length){
+    const apiDiet = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=100`);
     console.log({apiDiet: apiDiet.data});
     const infoDiet = apiDiet.data
     const diets = infoDiet.results.map((d) => d.name)
 
-    // diets.map((diet) => {
-    //     Diet.findOrCreate({
-    //         where: {
-    //             name: diet
+    diets.map((diet) => {
+        Diet.findOrCreate({
+            where: {
+                name: diet
 
-    //         }
-    //     })
-    // })
-    // const allDiets = await Diet.findAll();
-    // return allDiets
-    //  };
+            }
+        })
+    })
+    const allDiets = await Diet.findAll();
+    return allDiets
+     };
 
     return {"succes": true}
 
